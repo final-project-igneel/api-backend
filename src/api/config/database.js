3755238c5fa23a7710282a44eb796343807e79c7
aditilham <aditilham152@gmail.com>
2019-07-29 18:17:28 +0700

    moving to production

--- src/api/config/database.js
+++ src/api/config/database.js
@@ -2,10 +2,10 @@ require("dotenv").config()
 
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME_DEV,
-    password: process.env.DB_PASSWORD_DEV,
-    database: process.env.DB_DEV,
-    host: process.env.DB_HOST_DEV,
+    username: process.env.DB_USERNAME_PROD,
+    password: process.env.DB_PASSWORD_PROD,
+    database: process.env.DB_PROD,
+    host: process.env.DB_HOST_PROD,
     dialect: "mysql",
     operatorsAliases: false
   },
