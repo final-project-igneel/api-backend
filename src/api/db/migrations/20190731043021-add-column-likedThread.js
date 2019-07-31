ffc981a86cddf88be9adecb311aded9f5980e108
aditilham <aditilham152@gmail.com>
2019-07-31 12:10:14 +0700

    Improve package json

--- src/api/db/migrations/20190731043021-add-column-likedThread.js
+++ src/api/db/migrations/20190731043021-add-column-likedThread.js
@@ -2,12 +2,17 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-   return queryInterface.addColumn("threads", "likedThread", {type: Sequelize.STRING, defaultValue: '0'}, {
-      after: "input"
-    });
+    return queryInterface.addColumn(
+      "threads",
+      "likedThread",
+      { type: Sequelize.STRING, defaultValue: "0" },
+      {
+        after: "input"
+      }
+    );
   },
 
   down: (queryInterface, Sequelize) => {
-   return queryInterface.removeColumn("threads", "likedThread")
+    return queryInterface.removeColumn("threads", "likedThread");
   }
 };
