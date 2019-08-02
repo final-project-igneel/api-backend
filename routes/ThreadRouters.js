24bb23b6a0a885ae9c8037e69008dfa0aee68483
aditilham <aditilham152@gmail.com>
2019-08-02 09:22:08 +0700

    removing search thread

--- routes/ThreadRouters.js
+++ routes/ThreadRouters.js
@@ -16,6 +16,5 @@ router.post("/create", createThread);
 router.put("/:id", updateThreadLike);
 router.put("/edit/:id", editThread);
 router.delete("/delete/:id", deleteThread);
-router.get("/search", searchThread);
 
 module.exports = router;
